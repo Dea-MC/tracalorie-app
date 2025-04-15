3e02fcbfd26bcb4614f44391a9d5e02e8f207078
Dea Cruz <cruzdeamariz@gmail.com>
2025-04-15 21:40:59 +0400

    Clear Local Storage

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ class CalorieTracker {
         this._displayCaloriesBurned();
         this._displayCaloriesRemaining();
         this._displayCaloriesProgress();
+
+        document.getElementById('limit').value = this._calorieLimit;
     }
 
     // Public Methods/API // 
@@ -63,6 +65,7 @@ class CalorieTracker {
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
+        Storage.clearAll();
         this._render();
     }
 
@@ -301,6 +304,14 @@ class Storage {
         const workouts = Storage.getWorkouts();
         workouts.push(workout);
         localStorage.setItem('workouts', JSON.stringify(workouts));
+    }
+
+    static clearAll () {
+        localStorage.removeItem('totalCalories');
+        localStorage.removeItem('meals');
+        localStorage.removeItem('workouts');
+        // If you want to also remove Calorie Limit
+        // localStorage.clear()
     }
 }
 
